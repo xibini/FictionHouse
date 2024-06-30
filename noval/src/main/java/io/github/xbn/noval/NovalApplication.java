3801bb3c151a689ab545d814addfd8ee6322e518
xibini <3296952290@qq.com>
2024-06-30 17:28:58 +0800

    初始化项目

---
+++ noval/src/main/java/io/github/xbn/noval/NovalApplication.java
@@ -0,0 +1,13 @@
+package io.github.xbn.noval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovalApplication.class, args);
+    }
+
+}
---
+++ noval/src/test/java/io/github/xbn/noval/NovalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xbn.noval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
